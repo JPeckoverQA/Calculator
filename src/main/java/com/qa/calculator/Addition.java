eeaa591fe67ef7e875d20c2013a20d78a6b75e9f
DWONeill18 <d.w.oneill.18@gmail.com>
2019-05-29 14:21:06 +0100

    addition method included

---
+++ src/main/java/com/qa/calculator/Addition.java
@@ -0,0 +1,16 @@
+package src.main.java.com.qa.calculator;
+
+public class Addition extends Calculator {
+	
+	public double add(double num1, double num2) {
+	
+		
+		double output1 = getNum1() + getNum2();
+		
+//		float output1 = Num1 + Num2;
+		
+		return output1;
+		
+	}
+
+}
