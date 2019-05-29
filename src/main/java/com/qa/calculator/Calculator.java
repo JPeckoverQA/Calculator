c843cdcda11c305ccc217a34b7981fd6b6e68470
Johnny <Jonathan.Peckover@academytrainee.com>
2019-05-29 12:55:13 +0100

    slight but ineffective edit to Calculator

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -6,10 +6,13 @@ public class Calculator {
 //	}
 	private float num1;
 	private float num2;
-	
+	private String operator;
 	
 	
 	public void calculate() {
+		
+		setOperator(Utilities.input("What operation would you like to do? +, -, * or /")) ; 
+		
 		if( Utilities.input("What operation would you like to do? +, -, * or /").equals("+")) {
 			Addition.add();
 		}
@@ -29,6 +32,14 @@ public class Calculator {
 
 	public void setNum2(float num2) {
 		this.num2 = num2;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
 	}
 	
 }
